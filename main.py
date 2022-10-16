2ab61b368e5a7f31797e8827582353d2bd513020
hubertkulak <93318546+hubertkulak@users.noreply.github.com>
2022-10-16 13:16:27 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,23 @@
-import pygame 
+import pygame
+from sys import exit
+pygame.init()
+ekran = pygame.display.set_mode((1920,1080))
+pygame.display.set_caption('HIGH RIDE')
+czas = pygame.time.Clock()
 
-def main():
-    print("123")
+background = pygame.image.load('/home/robert/cargame/venv/textures/tlogry.png')
+car = pygame.image.load('/home/robert/cargame/venv/textures/auto.png')
 
-if __name__ == "__main__":
-    main()
+backgroundsurface = pygame.Surface((1920,1080))
+
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
+
+    ekran.blit(background,(0,0))
+
+    pygame.display.update()
+    czas.tick(60)
