99f3df8358c75f449316d57041dc3f2b3650ef0a
hubertkulak <93318546+hubertkulak@users.noreply.github.com>
2022-10-17 14:42:09 +0200

    dodanie baneru

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ game_screen_res = settings.screen_resolution[resolutuion_option]
 SCREEN_WIDTH = game_screen_res[0]
 SCREEN_HEIGHT = game_screen_res[1]
 car_scale = settings.car_rescale[resolutuion_option]
+baner_scale = settings.baner_rescale[resolutuion_option]
 
 pygame.init()
 ekran = pygame.display.set_mode(game_screen_res)
@@ -22,8 +23,11 @@ background = pygame.image.load('textures/tlogry.png')
 background = pygame.transform.scale(background, game_screen_res)
 car_surface = pygame.image.load('textures/auto.png')
 car_surface = pygame.transform.rotozoom(car_surface, 180, car_scale)
-car_rectangle = car_surface.get_rect(center = (game_screen_res[0] // 2, game_screen_res[1]  - 50))
+baner = pygame.image.load('textures/baner.png')
+baner = pygame.transform.rotozoom(baner,0,car_scale)
 
+car_rectangle = car_surface.get_rect(center = (game_screen_res[0]-730, game_screen_res[1]  - 100))
+baner_rectangle = baner.get_rect(center =(game_screen_res[0]-250,game_screen_res[1]-400))
 backgroundsurface = pygame.Surface(game_screen_res)
 
 
@@ -37,6 +41,6 @@ while True:
 
     ekran.blit(background,(0,0))
     ekran.blit(car_surface, car_rectangle)
-
+    ekran.blit(baner,baner_rectangle)
     pygame.display.update()
     czas.tick(60)
