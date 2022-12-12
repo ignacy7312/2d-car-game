de0a84ca70b16918ef37e559575cf949a6fd2740
pochodyla <pochodyla@gmail.com>
2022-12-12 20:37:53 +0100

    garaz

--- screen/garage_module.py
+++ screen/garage_module.py
@@ -14,7 +14,9 @@ class Garage(GameScreen):
         super().__init__(w, h)
         
         self.coins = ogar_db.get_coins(ogar_db.create_connection('baza2.db').cursor())
-        
+        # to musi pobierać z bazy danych: !!!!
+        self.is_unlocked = [1,0,0]
+
         self.text1 = self.font.render("select your supercar", True, 'black')
         self.text_rect1 = self.text1.get_rect(center = (self.w//2, 100))
         
@@ -28,6 +30,14 @@ class Garage(GameScreen):
         self.select_button = pygame.transform.rotozoom(self.select_button, 0, 0.6)
         self.select_button_rect = self.select_button.get_rect(center = (300, self.h//2 + 150))
 
+        self.locked_button = pygame.image.load('textures/buttons/lockedb.png').convert_alpha()
+        self.locked_button = pygame.transform.rotozoom(self.locked_button, 0, 0.6)
+        self.locked_button_rect = self.locked_button.get_rect(center = (300, self.h//2 + 100))
+
+        self.buy_button = pygame.image.load('textures/buttons/buybtn.png').convert_alpha()
+        self.buy_button = pygame.transform.rotozoom(self.buy_button, 0, 0.6)
+        self.buy_button_rect = self.buy_button.get_rect(center = (300, self.h//2 + 150))
+
         self.l_arr = pygame.image.load('textures/buttons/larr.png').convert_alpha()
         self.l_arr_rect = self.l_arr.get_rect(center = (25, self.h//2))
         self.r_arr = pygame.image.load('textures/buttons/rarr.png').convert_alpha()
@@ -36,15 +46,17 @@ class Garage(GameScreen):
         self.car1 = pygame.image.load('textures/gcars/rcar.png').convert_alpha()
         self.car1_rect = self.car1.get_rect(center = (self.w//2, self.h//2))
         self.car1_text = self.font.render("nissan gtr", True, 'black')
+        
 
         self.car2 = pygame.image.load('textures/gcars/yelcar.png').convert_alpha()
         self.car2_rect = self.car2.get_rect(center = (self.w//2, self.h//2))
         self.car2_text = self.font.render("Lambo", True, 'black')
+        
 
         self.car3 = pygame.image.load('textures/gcars/supra.png').convert_alpha()
         self.car3_rect = self.car3.get_rect(center = (self.w//2, self.h//2))
         self.car3_text = self.font.render("toyota Supra", True, 'black')
-
+        
         
         
 
@@ -89,13 +101,19 @@ class Garage(GameScreen):
     def get_selected_car(self):
         return self.selected_car       
 
+    def display_select_button(self):
+        ''' Funckja która odpowiednio wyświetla przycisk wyboru/blokady i zakupu auta'''
+        if self.is_unlocked[self.curr_car]:
+            self.screen.blit(self.select_button, self.select_button_rect)
+        else:
+            self.screen.blit(self.locked_button, self.locked_button_rect)
+            self.screen.blit(self.buy_button, self.buy_button_rect)
     
     def display_buttons(self):
         self.screen.blit(self.menu_button, self.menu_button_rect)
-        self.screen.blit(self.select_button, self.select_button_rect)
         self.screen.blit(self.l_arr, self.l_arr_rect)
         self.screen.blit(self.r_arr, self.r_arr_rect)
-                    
+        self.display_select_button()                    
         
     def display_garage(self):
         self.screen.fill('brown')
